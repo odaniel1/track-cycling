3752348022e2fdfdb35ebca5ceafb15b8ff8e662
odaniel1 <owendnl@gmail.com>
2021-07-18 15:50:40 +0100

    static branching

--- _targets.R
+++ _targets.R
@@ -6,17 +6,22 @@
 source("R/packages.R")
 purrr::map(dir_ls("R/functions", recurse = TRUE, glob = "*.R"), source)
 
+models <- tribble(
+  ~model_name, ~path, ~description,
+  "bt1", "stan/bt1.stan", "Basic Bradley Terry model.",
+  "bt1.1", "stan/bt1.1.stan", "bt1, with match log loss",
+  "bt2", "stan/bt2.stan", "BT with match log density",
+  "bt3", "stan/bt3.stan", "bt2, with home advantage effect",
+  # "bt3.2", "stan/bt3.2.stan", "bt2, with Gaussian Process for time changing strengths"
+)
 
+## ---- DATA PREPARATION ----------------------------------------------------
 list(
-  
-  ## ---- LOCAL PATHS ---------------------------------------------------------
   
   tar_target(race_path, "../tissot-scraper/data/DONT-EDIT-race-lookup.csv", format = 'file'),
   
   tar_target(team_path, "../tissot-scraper/data/team-lookup.csv", format = 'file'),
 
-  ## ---- DATA PREPARATION ----------------------------------------------------
-  
   tar_target(races, prepare_races(race_path)),
   
   tar_target(results, prepare_results(races)),
@@ -31,50 +36,17 @@ list(
   
   tar_target(pairings, prepare_pairings(matches)),
   
-  
-  ## ---- STAN MODELS ---------------------------------------------------------
-  
   tar_target(stan_data, prepare_stan_data(riders, matches, pairings, rider_days)),
   
-  tar_stan_mcmc(
-    name = bt1,
-    stan_files = "stan/bt1.stan",
+## ---- STAN MODELS --------------------------------------------------------- 
+tar_stan_mcmc(
+    name = bt,
+    stan_files = models$path,
     data = stan_data,
     iter_warmup = 999, iter_sampling = 1000,
     parallel_chains = 4,
-    seed = 1414214,
-    refresh = 500
-  ),
-
-  tar_stan_mcmc(
-    name = bt1.1,
-    stan_files = "stan/bt1.1.stan",
-    data = stan_data,
-    iter_warmup = 999, iter_sampling = 1000,
-    parallel_chains = 4,
-    seed = 1414214,
-    refresh = 500
-  ),
-
-  tar_stan_mcmc(
-    name = bt2,
-    stan_files = "stan/bt2.stan",
-    data = stan_data,
-    iter_warmup = 999, iter_sampling = 1000,
-    parallel_chains = 4,
-    seed = 1414214,
-    refresh = 500
-  ),
-  
-  tar_stan_mcmc(
-    name = bt3.2,
-    stan_files = "stan/bt3.2.stan",
-    data = stan_data,
-    iter_warmup = 999, iter_sampling = 1000,
-    parallel_chains = 4, chains = 4,
     seed = 1414214,
     refresh = 500
   )
 )
-
 
